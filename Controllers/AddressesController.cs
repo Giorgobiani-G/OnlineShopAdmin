8a3ba32b04e10e4c67b270c4b858162fbd446321
admin <admin@DESKTOP-LILFTDD>
2022-09-18 23:28:36 +0400

    productcategoriesController

--- Controllers/AddressesController.cs
+++ Controllers/AddressesController.cs
@@ -49,9 +49,7 @@ namespace OnlineShopAdmin.Controllers
             return View();
         }
 
-        // POST: Addresses/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+     
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Address address, CancellationToken cancellationToken = default)
@@ -80,9 +78,7 @@ namespace OnlineShopAdmin.Controllers
             return View(address);
         }
 
-        // POST: Addresses/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, Address address, CancellationToken cancellationToken = default)
--- Controllers/ProductCategoriesController.cs
+++ Controllers/ProductCategoriesController.cs
@@ -1,48 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using OnlineShopAdmin.DataAccess.DbContexts;
 using OnlineShopAdmin.DataAccess.Models;
 using OnlineShopAdmin.DataAccess.Repository;
 using OnlineShopAdmin.Filters;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace OnlineShopAdmin.Controllers
 {
     [HttpRequestInfo]
     public class ProductCategoriesController : Controller
     {
-        private readonly AdventureWorksLT2019Context _context;
+         
         private readonly IRepository<ProductCategory> _productCategoryRepository;
 
 
-        public ProductCategoriesController(AdventureWorksLT2019Context context, IRepository<ProductCategory> productCategoryRepository)
+        public ProductCategoriesController(IRepository<ProductCategory> productCategoryRepository)
         {
-            _context = context;
             _productCategoryRepository = productCategoryRepository;
         }
 
         // GET: ProductCategories
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(CancellationToken cancellationToken = default)
         {
-            var adventureWorksLT2019Context = _context.ProductCategories.Include(p => p.ParentProductCategory).Include(p=>p.Products);
-            return View(await adventureWorksLT2019Context.ToListAsync());
+            return View(await _productCategoryRepository.GetListAsync(cancellationToken, new string[] { "ParentProductCategory", "Products" }));
         }
 
         // GET: ProductCategories/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Details(int? id, CancellationToken cancellationToken = default)
         {
             if (id == null)
             {
                 return NotFound();
             }
+            var productCategory = await _productCategoryRepository.GetByIdAsync((int)id, cancellationToken, new string[] { "ParentProductCategory" });
 
-            var productCategory = await _context.ProductCategories
-                .Include(p => p.ParentProductCategory)
-                .FirstOrDefaultAsync(m => m.ProductCategoryId == id);
+            //var productCategory = await _context.ProductCategories
+            //    .Include(p => p.ParentProductCategory)
+            //    .FirstOrDefaultAsync(m => m.ProductCategoryId == id);
             if (productCategory == null)
             {
                 return NotFound();
@@ -52,9 +50,9 @@ namespace OnlineShopAdmin.Controllers
         }
 
         // GET: ProductCategories/Create
-        public IActionResult Create()
+        public IActionResult Create(CancellationToken cancellationToken = default)
         {
-            ViewData["ParentProductCategoryId"] = new SelectList(_context.ProductCategories, "ProductCategoryId", "Name");
+            ViewData["ParentProductCategoryId"] = new SelectList(_productCategoryRepository.GetList(), "ProductCategoryId", "Name");
             return View();
         }
 
@@ -63,41 +61,38 @@ namespace OnlineShopAdmin.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ProductCategoryId,ParentProductCategoryId,Name,Rowguid,ModifiedDate")] ProductCategory productCategory)
+        public async Task<IActionResult> Create(ProductCategory productCategory, CancellationToken cancellationToken = default)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(productCategory);
-                await _context.SaveChangesAsync();
+                await _productCategoryRepository.InseretAsynch(productCategory, cancellationToken);
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["ParentProductCategoryId"] = new SelectList(_context.ProductCategories, "ProductCategoryId", "Name", productCategory.ParentProductCategoryId);
+            ViewData["ParentProductCategoryId"] = new SelectList(_productCategoryRepository.GetList(), "ProductCategoryId", "Name", productCategory.ParentProductCategoryId);
             return View(productCategory);
         }
 
         // GET: ProductCategories/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public async Task<IActionResult> Edit(int? id, CancellationToken cancellationToken = default)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var productCategory = await _context.ProductCategories.FindAsync(id);
+            var productCategory = await _productCategoryRepository.GetByIdAsync((int)id, cancellationToken);
             if (productCategory == null)
             {
                 return NotFound();
             }
-            ViewData["ParentProductCategoryId"] = new SelectList(_context.ProductCategories, "ProductCategoryId", "Name", productCategory.ParentProductCategoryId);
+            ViewData["ParentProductCategoryId"] = new SelectList(_productCategoryRepository.GetList(), "ProductCategoryId", "Name", productCategory.ParentProductCategoryId);
             return View(productCategory);
         }
 
-        // POST: ProductCategories/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ProductCategoryId,ParentProductCategoryId,Name,Rowguid,ModifiedDate")] ProductCategory productCategory)
+        public async Task<IActionResult> Edit(int id, ProductCategory productCategory, CancellationToken cancellationToken = default)
         {
             if (id != productCategory.ProductCategoryId)
             {
@@ -108,12 +103,11 @@ namespace OnlineShopAdmin.Controllers
             {
                 try
                 {
-                    _context.Update(productCategory);
-                    await _context.SaveChangesAsync();
+                    await _productCategoryRepository.UpdateAsynch(productCategory, cancellationToken);
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!ProductCategoryExists(productCategory.ProductCategoryId))
+                    if (!await _productCategoryRepository.CustomExists(productCategory.ProductCategoryId))
                     {
                         return NotFound();
                     }
@@ -124,21 +118,20 @@ namespace OnlineShopAdmin.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["ParentProductCategoryId"] = new SelectList(_context.ProductCategories, "ProductCategoryId", "Name", productCategory.ParentProductCategoryId);
+            ViewData["ParentProductCategoryId"] = new SelectList(_productCategoryRepository.GetList(), "ProductCategoryId", "Name", productCategory.ParentProductCategoryId);
             return View(productCategory);
         }
 
         // GET: ProductCategories/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> Delete(int? id, CancellationToken cancellationToken = default)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var productCategory = await _context.ProductCategories
-                .Include(p => p.ParentProductCategory)
-                .FirstOrDefaultAsync(m => m.ProductCategoryId == id);
+            var productCategory = await _productCategoryRepository.GetByIdAsync((int)id, cancellationToken, new string[] { "ParentProductCategory" });
+
             if (productCategory == null)
             {
                 return NotFound();
@@ -150,17 +143,10 @@ namespace OnlineShopAdmin.Controllers
         // POST: ProductCategories/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(int id, CancellationToken cancellationToken = default)
         {
-            var productCategory = await _context.ProductCategories.FindAsync(id);
-            _context.ProductCategories.Remove(productCategory);
-            await _context.SaveChangesAsync();
+            await _productCategoryRepository.DeleteAsynch(id, cancellationToken);
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool ProductCategoryExists(int id)
-        {
-            return _context.ProductCategories.Any(e => e.ProductCategoryId == id);
         }
     }
 }
